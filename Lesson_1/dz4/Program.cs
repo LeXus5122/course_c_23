1f1c4b394ab18ddf0a514f8b7abb3c479b4e07e1
LeXus22 <lexus22@inbox.ru>
2023-02-28 20:27:52 +0300

    dz4

---
+++ Lesson_1/dz4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Цифра 1 = 1, введите цифру 2: ");
+        int m = int.Parse(Console.ReadLine()!);
+        for (int i = 1; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
