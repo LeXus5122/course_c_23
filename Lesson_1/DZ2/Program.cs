bd5da2aec1f62fe7657342889af1eaa3dc9caeb3
LeXus22 <lexus22@inbox.ru>
2023-02-28 17:28:45 +0300

    dz2

---
+++ Lesson_1/DZ2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+
+if (a > b &  a > c)
+{
+max = a;
+}
+
+else if (b > c & b > a)
+{
+   max = b;
+}
+
+else if (c > a & c > b)
+{
+   max = c;
+}
+
+Console.WriteLine(max);
