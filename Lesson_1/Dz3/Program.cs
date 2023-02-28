26a4c4f3ca86c556e576ce3a1e73cc50201b8ee0
LeXus22 <lexus22@inbox.ru>
2023-02-28 19:45:35 +0300

    dz3

---
+++ Lesson_1/Dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+if ( a %2 == 0 )
+{
+    Console.WriteLine("Da");
+}
+else
+{
+    Console.WriteLine("Net");
+}
