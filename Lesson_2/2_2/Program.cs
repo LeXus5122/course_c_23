2686bce94ca524150e360d2dd38942733fecb01c
LeXus22 <lexus22@inbox.ru>
2023-03-01 13:54:55 +0300

    2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Генерируется число");
+//int num = new Random().Next(10,99);
+//int a = num / 10
+//Console.WriteLine(a);
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num % 10;
+    if (num1 > num2) return num1;
+    return num2;
+}
+
+int result = TakeNum(new Random().Next(10, 99));
+Console.WriteLine(result);
