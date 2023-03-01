3b26b0c84f21f93207ed1a5b0de25b2d126269c4
LeXus22 <lexus22@inbox.ru>
2023-03-01 13:43:58 +0300

    2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Введите число");
+//int x = int.Parse(Console.ReadLine()!);
+//num = x % 10
+//Console.WriteLine(num);
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+//Console.WriteLine(num % 10);
