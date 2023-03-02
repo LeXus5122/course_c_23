5de0b657dae82f413fb3185d241c7955ea9c2fa5
LeXus22 <lexus22@inbox.ru>
2023-03-03 00:49:42 +0300

    dz3pravka

--- Lesson_2/DZ3/Program.cs
+++ Lesson_2/DZ3/Program.cs
@@ -1,50 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Console.WriteLine("Введите число: ");
-// int num = int.Parse(Console.ReadLine()!);
-// int digit = 0;
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
 
-// if (num > 99)
+// if (number >= 100 && number <= 999) 
 // {
-//     digit = (num / 100) % 10;
-// }
-// else{
-//     Console.WriteLine("В числе меньше трех цифр");
-// }
-// if (digit != 0)
+//     int num1 = number % 10;
+//     Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
+// } 
+
+// else if (number >= 1000 && number <= 9999) 
 // {
-//     Console.WriteLine("Третья цифры в числе: " + digit);
+//     int num1 = (number / 10) % 10;
+//     Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
+// } 
+
+// else if (number >= 10000 && number <= 99999) 
+// {
+//     int num1 = (number / 100) % 10;
+//     Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
+// } 
+
+// else if (number > 99999) 
+// {
+//     Console.WriteLine("Некорректное число");
 // }
 
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+// else if (number < 100) 
+// {
+//     Console.WriteLine("Нет третьей цифры");
+// }
 
-if (number >= 100 && number <= 999) 
-{
-    int num1 = number % 10;
-    Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
-} 
 
-else if (number >= 1000 && number <= 9999) 
+void ThirdNum(int num)
 {
-    int num1 = (number / 10) % 10;
-    Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
-} 
+    Console.Write($" {num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("there is no third digit");
+    return;
+    }
 
-else if (number >= 10000 && number <= 99999) 
-{
-    int num1 = (number / 100) % 10;
-    Console.WriteLine("Третья цифра числа " + number + " равна " + num1);
-} 
-
-else if (number > 99999) 
-{
-    Console.WriteLine("Некорректное число");
+while (num > 999) num /= 10;
+Console.WriteLine(num % 10);
 }
 
-else if (number < 100) 
-{
-    Console.WriteLine("Нет третьей цифры");
-}
-
-
+ThirdNum(int.Parse(Console.ReadLine()!));
