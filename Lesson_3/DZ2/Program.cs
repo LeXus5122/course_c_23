587e708881dab6787c1f7d282a23697b6eacef76
LeXus22 <lexus22@inbox.ru>
2023-03-03 02:37:03 +0300

    vvod dannix

--- Lesson_3/DZ2/Program.cs
+++ Lesson_3/DZ2/Program.cs
@@ -5,4 +5,13 @@ double dis(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 }
-Console.WriteLine(Math.Round(dis(7, -5, 0, 1, -1, 9)));
+
+Console.Write("Введите поочередно значения x1, y1, z1, x2, y2, z2: ");
+int x1 = int.Parse(Console.ReadLine()!);
+int y1 = int.Parse(Console.ReadLine()!);
+int z1 = int.Parse(Console.ReadLine()!);
+int x2 = int.Parse(Console.ReadLine()!);
+int y2 = int.Parse(Console.ReadLine()!);
+int z2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Math.Round(dis(x1, y1, z1, x2, y2, z2)) + " (Значение округлено)");
