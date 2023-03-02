1b842d3e5a476a5fe589c5d9f278f564d4e0d694
LeXus22 <lexus22@inbox.ru>
2023-03-02 12:16:23 +0300

    dz1

--- Lesson_2/2_4/Program.cs
+++ Lesson_2/2_4/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine()!);
+int b = 7;
+int c = 23;
 
 void Div(int num1, int num2, int num3)
 {
@@ -8,14 +11,5 @@ void Div(int num1, int num2, int num3)
         Console.WriteLine("Не кратно " + num1 % num2);
     }
 }
-
-// int num3 = int.Parse(Console.ReadLine()!);
-// int num4 = int.Parse(Console.ReadLine()!);
-// int result = Div( num3, num4 );
-// return result;
-
-int a = int.Parse(Console.ReadLine()!);
-int b = 7;
-int c = 23;
 
 Div(a, b, c);
---
+++ Lesson_2/DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Введите число");
+//int x = int.Parse(Console.ReadLine()!);
+//num = x % 10
+//Console.WriteLine(num);
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num / 10 % 10;
+}
+int Text = int.Parse(Console.ReadLine()!);
+int result = TakeNum(Text);
+Console.WriteLine(result);
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+//Console.WriteLine(num % 10);
