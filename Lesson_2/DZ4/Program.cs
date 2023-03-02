69e1e9bc06bd330b9889d5c1503d7846bbdbd8d7
LeXus22 <lexus22@inbox.ru>
2023-03-02 19:35:36 +0300

    dz4

---
+++ Lesson_2/DZ4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine()!);
+if (day == 6 | day == 7) 
+{
+    Console.WriteLine("Это выходной день!");
+} 
+else 
+{
+    Console.WriteLine("Это рабочий день.");
+}
