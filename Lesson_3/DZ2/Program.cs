27966adaff4f7ed1e09f99dae522e93fc7aeedc1
LeXus22 <lexus22@inbox.ru>
2023-03-03 01:55:30 +0300

    dz2

---
+++ Lesson_3/DZ2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Расстояние = $ \ sqrt {(x2-x1) ^{2} + (y2-y1) ^{2} + (z2-z1) ^{2}} $   
+// и округлить числа
+
+double dis(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+Console.WriteLine(Math.Round(dis(7, -5, 0, 1, -1, 9)));
