f33bdc3d617a055909d946d35e396bfca1ea3ec0
LeXus22 <lexus22@inbox.ru>
2023-03-10 21:29:15 +0300

    dz2

---
+++ Lesson_4/DZ2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число, сумму цифр которых нужно посчитать: ");
+int y = int.Parse(Console.ReadLine()!);
+            int x = 0;
+            while (y > 0)
+            {
+ 
+                x = x + y % 10;
+                y = y /10 ;
+ 
+            }
+ Console.WriteLine(x);
