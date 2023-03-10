a7c1c717dfd53a420063bfb64d6d9b6cebfbddfb
LeXus22 <lexus22@inbox.ru>
2023-03-10 22:00:06 +0300

    dz3

---
+++ Lesson_4/DZ3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите число массив с которым необходимо создать: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начала рандомного диапазона: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конец рандомного диапазона: ");
+int z = int.Parse(Console.ReadLine()!);
+
+void FillArray(int[] collection)
+
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(y, z);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+
+int[] array = new int[x];
+
+FillArray(array);
+PrintArray(array);
