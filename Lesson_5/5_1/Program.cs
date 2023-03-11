c87e0993c5fd44f3611b25519d9c65db7e9534f0
LeXus22 <lexus22@inbox.ru>
2023-03-11 14:02:34 +0300

    5_1

---
+++ Lesson_5/5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] InputRandomMassive(int lenghtMas, int minRangeMas, int maxRangeMas)
+{
+    int[] masRandom = new int[lenghtMas];
+    for (int i = 0; i < lenghtMas; i++)
+    {
+        masRandom[i] = new Random().Next(minRangeMas, maxRangeMas);
+    }
+    return masRandom;
+}
+
+void PrintMassive(int[] mas)
+{
+    for (int i = 0; i<mas.Length; i++)
+        Console.Write($" {mas[i]} ");
+    Console.WriteLine();
+}
+
+void MassConverter(int[] arry)
+{
+    for (int i=0; i<arry.Length; i++)
+    arry[i]=-arry[i];
+}
+
+Console.WriteLine("Введите длину массива:");
+int lenghtMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное значение для диапазона случайного числа: ");
+int minRangeMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапазона случайного числа: ");
+int maxRangeMas = int.Parse(Console.ReadLine()!);
+
+int[] masRandom = InputRandomMassive(lenghtMas, minRangeMas, maxRangeMas);
+PrintMassive(masRandom);
+MassConverter(masRandom);
+PrintMassive(masRandom);
