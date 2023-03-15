f3ea89d8eb6447928d9f328fcdcd0ba08a51a727
LeXus22 <lexus22@inbox.ru>
2023-03-15 13:19:54 +0300

    dz2

---
+++ Lesson_6/dz2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значения b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значения k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значения b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значения k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения: ({x}, {y})");
