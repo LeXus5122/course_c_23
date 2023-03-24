fe38dacddf605f4097ce00ee81a8381bac26ab7e
LeXus22 <lexus22@inbox.ru>
2023-03-24 11:44:11 +0300

    dz8

---
+++ Lesson_8/dz2/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] matrix = {
+                {1, 4, 7, 2},
+                {5, 9, 2, 3},
+                {8, 4, 2, 4},
+                {5, 2, 6, 7}
+            };
+
+int minSum = int.MaxValue;
+int minSumRow = -1;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int rowSum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        rowSum += matrix[i, j];
+    }
+
+    if (rowSum < minSum)
+    {
+        minSum = rowSum;
+        minSumRow = i;
+    }
+}
+
+Console.WriteLine($"Строка с наименьшей суммой элементов: {minSumRow} (сумма: {minSum})");
---
+++ Lesson_8/dz4/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] matrix = {
+                { 5, 2, 4, 6 },
+                { 1, 6, 3, 7 },
+                { 8, 7, 9, 3 }
+                };
+
+int minRow = 0;
+int minCol = 0;
+int minValue = matrix[0, 0];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] < minValue)
+        {
+            minValue = matrix[i, j];
+            minRow = i;
+            minCol = j;
+        }
+    }
+}
+
+int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+
+for (int i = 0, newRow = 0; i < matrix.GetLength(0); i++)
+{
+    if (i != minRow)
+    {
+        for (int j = 0, newCol = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j != minCol)
+            {
+                newMatrix[newRow, newCol] = matrix[i, j];
+                newCol++;
+            }
+        }
+        newRow++;
+    }
+}
+
+Console.WriteLine("Новая матрица после удаления строки и столбца с минимальным элементом:");
+for (int i = 0; i < newMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < newMatrix.GetLength(1); j++)
+    {
+        Console.Write(newMatrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
